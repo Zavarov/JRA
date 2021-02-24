1609ceca47494542489492a8edd7eec1734807cc
Zavarov <25961215+Zavarov@users.noreply.github.com>
2021-02-25 00:52:33 +0100

    Implement an iterator over listings.

--- core/src/main/java/vartas/jra/models/Listing.java
+++ core/src/main/java/vartas/jra/models/Listing.java
@@ -1,11 +1,16 @@
 package vartas.jra.models;
 
+import com.google.common.collect.AbstractIterator;
+import com.google.common.collect.Iterables;
 import org.json.JSONObject;
+import org.slf4j.LoggerFactory;
+import vartas.jra.exceptions.HttpException;
 import vartas.jra.models._factory.ThingFactory;
 import vartas.jra.models._json.JSONListing;
+import vartas.jra.query.QueryGet;
 
 import javax.annotation.Nonnull;
-import java.util.Iterator;
+import java.io.IOException;
 import java.util.function.Function;
 
 public class Listing<V> extends ListingTOP implements Iterable<V>{
@@ -26,7 +31,39 @@ public class Listing<V> extends ListingTOP implements Iterable<V>{
 
     @Nonnull
     @Override
-    public Iterator<V> iterator() {
+    public java.util.Iterator<V> iterator() {
         return getChildren().stream().map(Object::toString).map(mapper).iterator();
+    }
+
+    public static class Iterator<V extends Snowflake> extends AbstractIterator<Listing<V>>{
+        private final QueryGet<Listing<V>> query;
+
+        public Iterator(QueryGet<Listing<V>> query){
+            this.query = query;
+        }
+
+        public static <V extends Snowflake> Iterator<V> from(QueryGet<Listing<V>> query){
+            return new Iterator<>(query);
+        }
+
+        @Override
+        protected Listing<V> computeNext() {
+            try {
+                Listing<V> listing = query.query();
+
+                if (listing.isEmptyChildren()) {
+                    return endOfData();
+                } else {
+                    //The last element of the current query serves
+                    //as an anchor point for the next query
+                    String after = Iterables.getLast(listing).getName();
+                    query.setParameter("after", after);
+                    return listing;
+                }
+            }catch(IOException | HttpException | InterruptedException e){
+                LoggerFactory.getLogger(getClass()).error(e.getMessage(), e);
+                return endOfData();
+            }
+        }
     }
 }
